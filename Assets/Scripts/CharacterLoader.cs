3ddf6bd77cbdef5ec1dba12ba7aa9e6298946390
Wesley Oliveira <32249497+Wesley-Oliveira@users.noreply.github.com>
2021-04-16 14:50:59 -0300

    Adding dissolve effects and adressables

---
+++ Assets/Scripts/CharacterLoader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class CharacterLoader : MonoBehaviour
+{
+    private GameObject characterPrefab;
+    public AssetReference characterReference;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            CreateCharacter();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(LoadCharacterFromRef());
+        }
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            InstantiateCharacterFromRef();
+        }
+    }
+
+    private void InstantiateCharacterFromRef()
+    {
+        characterReference.InstantiateAsync(Vector3.zero, Quaternion.identity);
+    }
+
+    private IEnumerator LoadCharacterFromRef()
+    {
+        var async = characterReference.LoadAssetAsync<GameObject>();
+
+        while (!async.IsDone)
+            yield return new WaitForEndOfFrame();
+
+        characterPrefab = async.Result;
+    }
+
+    private void CreateCharacter()
+    {
+        Addressables.LoadAssetAsync<GameObject>("Characters/PBRCharacter").Completed += OnLoadDone;
+    }
+
+    private void OnLoadDone(AsyncOperationHandle<GameObject> task)
+    {
+        characterPrefab = task.Result;
+        Instantiate(characterPrefab);
+    }
+}
